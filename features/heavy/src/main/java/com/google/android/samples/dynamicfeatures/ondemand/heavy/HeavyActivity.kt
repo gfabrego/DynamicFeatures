1eb23516b77e1e12fae45a4e9d06a9c207cffff5
gfabrego <gfabrego@gmail.com>
2019-01-30 10:20:26 +0100

    Add module with big images so it must be confirmed

---
+++ features/heavy/src/main/java/com/google/android/samples/dynamicfeatures/ondemand/heavy/HeavyActivity.kt
@@ -0,0 +1,12 @@
+package com.google.android.samples.dynamicfeatures.ondemand.heavy
+
+import android.os.Bundle
+import com.google.android.samples.dynamicfeatures.BaseSplitActivity
+
+class HeavyActivity : BaseSplitActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_heavy)
+    }
+}
